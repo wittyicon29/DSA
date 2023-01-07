d4b45a45d88a9230814d869fd952f205869f746f
Atharva <99320225+wittyicon29@users.noreply.github.com>
2023-01-07 12:00:19 +0530

    Add files via upload

---
+++ Course Schedule.java
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/course-schedule/description/
+
+class Solution{
+public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] adj = new ArrayList[numCourses];
+        for(int i=0; i<numCourses; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for(int[] pre : prerequisites) {
+            adj[pre[0]].add(pre[1]);
+        }
+        int[] visited = new int[numCourses];
+        for(int i=0; i<numCourses; i++) {
+            if( !dfs(i, visited, adj))
+                return false;
+        }
+        return true;
+    }
+    public boolean dfs(int node, int[] visited, ArrayList<Integer>[] adj) {
+        if(visited[node] == 1) {
+            return false;
+        }
+        if(visited[node] == 2) {
+            return true;
+        }
+        visited[node] = 1;
+        for(int n : adj[node]) {
+            if(!dfs(n, visited, adj))
+                return false;
+        }   
+        visited[node] = 2;
+        return true;
+    }
+} 
