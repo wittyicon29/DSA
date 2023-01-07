d371edcbc0fa64268e16455e0a8ec133956d303b
Atharva <99320225+wittyicon29@users.noreply.github.com>
2023-01-07 12:00:33 +0530

    Add files via upload

---
+++ Shortest Unsorted Continuous Subarray.java
@@ -0,0 +1,14 @@
+package 30Days-DSA.java.Microsoft;
+
+public class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length , begin = -1, end = -2, min = nums[n-1], max = nums[0];
+        for(int i = 1; i < n; i++){
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n-i-1]);
+            if(nums[i] < max) end = i;
+            if(nums[n-i-1] > min) begin = n-i-1;
+        }
+        return end - begin +1;
+    }
+}
