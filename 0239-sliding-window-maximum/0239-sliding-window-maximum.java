44525ef7b0312612a99758476c327c973b9b5ef6
Atharva <99320225+wittyicon29@users.noreply.github.com>
2023-08-16 19:53:43 +0530

    Time: 31 ms (82.85%), Space: 59.4 MB (70.89%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,43 +1,34 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-          int n=nums.length;
-          int ans[]=new int[n-k+1];
-          int z=0;
-          Stack<Integer> st=new Stack<>();
-          int nge[]=new int[n];
-          st.push(n-1);
-          nge[n-1]=n;
-          for(int i= n-2; i>=0;i--){
-            while(st.size()>0 && nums[i]>nums[st.peek()]){
-              st.pop();
-            }
-            if(st.size()==0){
-              nge[i]=n;
-              }
-              else{
-                nge[i]=st.peek();
-              }
-              st.push(i);
-            }
-            int j=0;
-          for(int i=0; i<n-k+1;i++){
-            if(j>=i+k){
-              j=i;
-            }
-            int max=nums[j];
-            while(j<i+k){
-              max=nums[j];
-              j=nge[j];
-              
-            }
-            ans[z++]=max;
-            }
-          return ans;
+    if (nums == null || nums.length == 0 || k <= 0) {
+        return new int[0];
     }
     
-    }
-
-
+    int n = nums.length;
+    int[] result = new int[n - k + 1];
+    Deque<Integer> deque = new LinkedList<>();
+    
+    for (int i = 0; i < n; i++) {
+        // remove elements that are outside the current sliding window
+        while (!deque.isEmpty() && deque.peek() < i - k + 1) {
+            deque.poll();
+        }
         
-
+        // remove elements that are smaller than the current element
+        while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+            deque.pollLast();
+        }
+        
+        // add the current element to the deque
+        deque.offer(i);
+        
+        // add the maximum element in the sliding window to the result
+        if (i - k + 1 >= 0) {
+            result[i - k + 1] = nums[deque.peek()];
+        }
+    }
     
+    return result;
+}
+
+}
