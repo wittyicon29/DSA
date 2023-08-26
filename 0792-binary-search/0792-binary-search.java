8becd2417db8760f0143030818498d534c12f831
Atharva <99320225+wittyicon29@users.noreply.github.com>
2023-08-26 15:49:06 +0530

    Time: 0 ms (100%), Space: 44.7 MB (64.49%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,20 +1,16 @@
 class Solution {
-    public int search(int[] nums, int target, int start, int end) {
-        if (start == end) return -1;
-
-        int mid = (start+end)/2;
-        if (nums[mid] == target) return mid;
-        
-        if (target < nums[mid]) 
-            return search(nums, target, start, mid);
-        else 
-            return search(nums, target, mid+1, end);
-    }
-    
     public int search(int[] nums, int target) {
-        int len = nums.length;
-        if (nums == null || len == 0 || target < nums[0] || target > nums[len-1]) 
-            return -1;
-        return search(nums, target, 0, len);
+        int low=0,high=nums.length-1;
+        while(low<=high)
+        {
+          int mid =(low+high)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return -1;
     }
 }
