11623ad2b356d3d0a71ed51c3017fcbab2ec2b80
Atharva <99320225+wittyicon29@users.noreply.github.com>
2023-08-26 15:34:47 +0530

    Time: 30 ms (66.07%), Space: 44.4 MB (40.46%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
@@ -1,15 +1,14 @@
-public class Solution {
+class Solution {
     public int partitionString(String s) {
-        int i = 0, ans = 1, flag = 0;
-        while(i < s.length()) {
-            int val = s.charAt(i) - 'a';
-            if ((flag & (1 << val)) != 0) {
-                flag = 0;
-                ans++;
-            }
-            flag = flag | (1 << val);
-            i++;
-        }
-        return ans;
+      HashSet<Character> set = new HashSet<>();
+      int count = 1;
+      for(char c : s.toCharArray()) {
+          if(set.contains(c)) {
+              count ++;
+              set.clear();
+          }
+          set.add(c);
+      }
+      return count;  
     }
 }
