ebecd01c3d0f3a89b251b248e4d59fcbcde2e8d8
Atharva <99320225+wittyicon29@users.noreply.github.com>
2024-07-01 22:47:30 +0530

    Time: 91 ms (7.63%), Space: 70.1 MB (67.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN; 
-        int currSum = 0;
+        return maxSubarrayHelper(nums, 0, nums.size() - 1);
+    }
 
-        for(int i = 0; i < nums.size(); i++) {
-            currSum += nums[i];
-            if(currSum > maxSum) {
-                maxSum = currSum;
-            }
-            if(currSum < 0) {
-                currSum = 0;
-            }
+private:
+    int maxSubarrayHelper(vector<int>& nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
         }
-        return maxSum;
+
+        int mid = (left + right) / 2;
+        int leftMax = maxSubarrayHelper(nums, left, mid);
+        int rightMax = maxSubarrayHelper(nums, mid + 1, right);
+        int crossMax = findCrossMaxSubarray(nums, left, mid, right);
+
+        return max(leftMax, max(rightMax, crossMax));
+    }
+
+    int findCrossMaxSubarray(vector<int>& nums, int left, int mid, int right) {
+        int leftSum = INT_MIN, rightSum = INT_MIN, currentSum = 0;
+
+        // Find the maximum subarray from the left half
+        for (int i = mid; i >= left; i--) {
+            currentSum += nums[i];
+            leftSum = max(leftSum, currentSum);
+        }
+
+        currentSum = 0;
+        // Find the maximum subarray from the right half
+        for (int i = mid + 1; i <= right; i++) {
+            currentSum += nums[i];
+            rightSum = max(rightSum, currentSum);
+        }
+
+        return leftSum + rightSum;
     }
 };
