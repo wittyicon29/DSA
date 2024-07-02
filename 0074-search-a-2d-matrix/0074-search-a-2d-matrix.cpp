74a2ec298e41f0185b3305d9ba65713fcb4df176
Atharva <99320225+wittyicon29@users.noreply.github.com>
2024-07-02 13:36:03 +0530

    Time: 3 ms (66.15%), Space: 11.9 MB (49.15%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
-        for (int i = 0; i < m; i++) {
-            // Perform binary search on the current row
-            if (binary_search(matrix[i].begin(), matrix[i].end(), target)) {
-                return true;
+        if(matrix.size() == 1 && matrix[0].size() == 1) return target == matrix[0][0];
+        if(matrix.size() == 1){
+            for(int i=0; i<matrix[0].size(); i++){
+                if(target == matrix[0][i])return true;
             }
+            return false;
         }
-        
+        if(matrix[0].size() == 1){
+            for(int i=0; i<matrix.size(); i++){
+                if(target == matrix[i][0]) return true;
+            }
+            return false;
+        }
+
+        int i=0;
+        int j=matrix[0].size()-1;
+
+        int currow = 0;
+        while(currow < matrix.size()){
+            if(target == matrix[currow][i] || target == matrix[currow][j]) return true;
+            else if(target > matrix[currow][i] && target < matrix[currow][j]){
+                while(i<j && i<matrix[0].size()){
+                    if(target == matrix[currow][i]) return true;
+                    else i++;
+                }
+                return false;
+            }
+            else if(target > matrix[currow][j]){
+                i=0;
+                currow++;
+            }
+            else return false;
+        }
         return false;
     }
 };
