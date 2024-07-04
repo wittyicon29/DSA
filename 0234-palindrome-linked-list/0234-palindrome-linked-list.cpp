cbeacd33fa51ffa6036cd8a5ab23f970b3dc3d26
Atharva <99320225+wittyicon29@users.noreply.github.com>
2024-07-04 12:43:02 +0530

    Time: 184 ms (30.49%), Space: 125.9 MB (24.43%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> stack;
+        ListNode* curr = head;
+        while (curr) {
+            stack.push(curr->val);
+            curr = curr->next;
+        }
+        curr = head;
+        while (curr && curr->val == stack.top()) {
+            stack.pop();
+            curr = curr->next;
+        }
+        return curr == nullptr;
+    }
+};
